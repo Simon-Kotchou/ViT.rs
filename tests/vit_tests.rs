52ac90bd591fae9a9fb38204f9ff95db594de889
Simon Kotchou <simonkotchou@mines.edu>
2024-04-17 20:08:58 -0700

    residual and matmul tests

--- tests/vit_tests.rs
+++ tests/vit_tests.rs
@@ -48,3 +48,85 @@ mod tests {
             assert!(!losses_slice.iter().all(|&x| x == 0.0));
         }
     }
+
+    #[test]
+    fn test_backward_pass() {
+        let mut vit = ViT::build_from_checkpoint("path/to/checkpoint.bin");
+        let b = 4;
+        let t = 64;
+        let v = vit.config.vocab_size;
+        let c = vit.config.channels;
+
+        let inputs = vec![0; b * t];
+        let targets = vec![0; b * t];
+        vit.forward(inputs.as_ptr(), targets.as_ptr(), b as c_int, t as c_int);
+
+        vit.backward();
+
+        unsafe {
+            assert_ne!(vit.grads.wte, std::ptr::null_mut());
+            assert_ne!(vit.grads.wpe, std::ptr::null_mut());
+            assert_ne!(vit.grads.ln1w, std::ptr::null_mut());
+            assert_ne!(vit.grads.ln1b, std::ptr::null_mut());
+            assert_ne!(vit.grads.qkvw, std::ptr::null_mut());
+            assert_ne!(vit.grads.qkvb, std::ptr::null_mut());
+            assert_ne!(vit.grads.attprojw, std::ptr::null_mut());
+            assert_ne!(vit.grads.attprojb, std::ptr::null_mut());
+            assert_ne!(vit.grads.ln2w, std::ptr::null_mut());
+            assert_ne!(vit.grads.ln2b, std::ptr::null_mut());
+            assert_ne!(vit.grads.fcw, std::ptr::null_mut());
+            assert_ne!(vit.grads.fcb, std::ptr::null_mut());
+            assert_ne!(vit.grads.fcprojw, std::ptr::null_mut());
+            assert_ne!(vit.grads.fcprojb, std::ptr::null_mut());
+            assert_ne!(vit.grads.lnfw, std::ptr::null_mut());
+            assert_ne!(vit.grads.lnfb, std::ptr::null_mut());
+
+            assert_ne!(vit.grads_acts.encoded, std::ptr::null_mut());
+            assert_ne!(vit.grads_acts.logits, std::ptr::null_mut());
+            assert_ne!(vit.grads_acts.probs, std::ptr::null_mut());
+            assert_ne!(vit.grads_acts.losses, std::ptr::null_mut());
+        }
+    }
+
+    #[test]
+    fn test_residual_forward() {
+        let n = 10;
+        let inp1 = vec![1.0; n];
+        let inp2 = vec![2.0; n];
+        let mut out = vec![0.0; n];
+
+        residual_forward(out.as_mut_ptr(), inp1.as_ptr(), inp2.as_ptr(), n as c_int);
+
+        assert_eq!(out, vec![3.0; n]);
+    }
+
+    #[test]
+    fn test_matmul_forward() {
+        let b = 2;
+        let t = 3;
+        let c = 4;
+        let oc = 5;
+
+        let inp = vec![1.0; b * t * c];
+        let weight = vec![2.0; oc * c];
+        let bias = vec![3.0; oc];
+        let mut out = vec![0.0; b * t * oc];
+
+        matmul_forward(
+            out.as_mut_ptr(),
+            inp.as_ptr(),
+            weight.as_ptr(),
+            bias.as_ptr(),
+            b as c_int,
+            t as c_int,
+            c as c_int,
+            oc as c_int,
+        );
+
+        let expected_out = vec![
+            35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0,
+            35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0, 35.0,
+            35.0, 35.0,
+        ];
+        assert_eq!(out, expected_out);
+    }
